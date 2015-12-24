01e8336e2e84011159c1426916d9328c3e791457
3soolo3 <soolo3@yandex.ru>
2015-12-24 14:46:05 +0300

    Update AutoSwitchersMain.pas
    Initialize in utilites

--- AutoSwitchersMain.pas
+++ AutoSwitchersMain.pas
@@ -1,7 +1,7 @@
 {~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
            AutoSwitchers - AIMP3 plugin
-                 Version: 1.2.1
+                 Version: 1.2.2
               Copyright (c) Lyuter
            Mail : pro100lyuter@mail.ru
 
@@ -18,7 +18,7 @@ uses  Windows, SysUtils,
       apiMUI;
 
 const
-    PLUGIN_NAME              = 'AutoSwitchers v1.2.1 for AIMP 3';
+    PLUGIN_NAME              = 'AutoSwitchers v1.2.2';
     PLUGIN_AUTHOR            = 'Author: Lyuter';
     PLUGIN_SHORT_DESCRIPTION = '';
     PLUGIN_FULL_DESCRIPTION  = '* The plugin automatically switch off the tracks ' +
@@ -346,10 +346,10 @@ end;
 Initialize}
 function TASPlugin.Initialize(Core: IAIMPCore): HRESULT;
 var
-  ServiceMenuManager: IAIMPServiceMenuManager;
+  APlaylistManager: IAIMPServicePlaylistManager;
   ServiceMessageDispatcher: IAIMPServiceMessageDispatcher;
 begin
-  Result := Core.QueryInterface(IID_IAIMPServiceMenuManager, ServiceMenuManager);
+  Result := Core.QueryInterface(IID_IAIMPServicePlaylistManager, APlaylistManager);
   if Succeeded(Result)
   then
     begin
